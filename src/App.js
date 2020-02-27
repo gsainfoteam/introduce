e108e64944149548fdb150c852f3c8181d951ba0
KIm, Hyeonseo <dodok8@gmail.com>
2020-02-27 18:01:54 +0900

    Add Material UI & basic font setting

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Navigation from './Navigation';
 import Section from './Section';
 import Footer from './Footer';
 import './App.css';
 
+const basicFontFamilty = createMuiTheme({
+  typography: {
+    fontFamily: ['Noto Sans KR'],
+  },
+});
+
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={basicFontFamilty}>
+      <CssBaseline />
+      말랑카우
       <header className="App-header">
         <h1>인포팀에 지원하시는 여러분을 환영합니다.</h1>
       </header>
       <Navigation></Navigation>
       <Section></Section>
       <Footer />
-    </div>
+    </ThemeProvider>
   );
 }
 
