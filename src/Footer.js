11e979b8028560b2dc59dbce429dc5aa4ace0339
KIm, Hyeonseo <dodok8@gmail.com>
2020-02-28 14:38:36 +0900

    Add additional copyright info

--- src/Footer.js
+++ src/Footer.js
@@ -9,6 +9,12 @@ const Footer = () => {
         {'이 페이지는'} <Link href="https://material-ui.com/">Material UI</Link>
         {'를 이용해 만들어 졌습니다.'}
         <br />
+        <Link href="https://material.io/resources/icons">Material Icons</Link>
+        {' are available under '}
+        <Link href="https://www.apache.org/licenses/LICENSE-2.0.html">
+          Apache license version 2.0
+        </Link>
+        <br />
         {'Copyright © '}GSA Infoteam {new Date().getFullYear()}
         {'.'}
       </Typography>
