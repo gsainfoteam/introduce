c8f6316f750aa6a3d3334f7d5b1b95ef7dd4571b
KIm, Hyeonseo <dodok8@gmail.com>
2020-02-29 21:17:28 +0900

    Update closed Dashboard space

--- src/DashBoard.js
+++ src/DashBoard.js
@@ -72,10 +72,7 @@ const useStyles = makeStyles(theme => ({
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
-    width: theme.spacing(7),
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9),
-    },
+    width: theme.spacing(0),
   },
   appBarSpacer: theme.mixins.toolbar,
   content: {
