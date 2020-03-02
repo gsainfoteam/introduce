616d471419882a701390e805ebe210c43de263a8
KIm, Hyeonseo <dodok8@gmail.com>
2020-03-03 00:43:27 +0900

    Update Checkout Forms

--- src/Checkout.js
+++ src/Checkout.js
@@ -7,9 +7,9 @@ import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+
 import AddressForm from './AddressForm';
 import PaymentForm from './PaymentForm';
-import axios from 'axios';
 
 const useStyles = makeStyles(theme => ({
   appBar: {
@@ -53,71 +53,44 @@ const steps = ['유의사항', '지원서 양식'];
 export default function Checkout() {
   const classes = useStyles();
   const [activeStep, setActiveStep] = React.useState(0);
-  const [understood, setUnderstood] = React.useState(false);
 
   const getStepContent = step => {
     switch (step) {
       case 0:
-        return <AddressForm nowUnderstand={nowUnderstand} />;
+        return (
+          <AddressForm
+            handleNext={handleNext}
+            isUnderstadning={false}
+            classes={classes}
+          />
+        );
       case 1:
-        return <PaymentForm updateApplyInfo={setApplyInfo} />;
+        return (
+          <PaymentForm
+            handleNext={handleNext}
+            handleBack={handleBack}
+            classes={classes}
+          />
+        );
       default:
         throw new Error('Unknown step');
     }
   };
 
-  const [applyInfo, setApplyInfo] = React.useState({
-    name: '',
-    student_id: '',
-    phone_number: '',
-    previous_dev_career: '',
-    message: '',
-  });
-
-  const sendMessage = () => {
-    axios({
-      url: 'https://infoteam_api.samhome.xyz/api/aplication',
-      method: 'POST',
-      data: applyInfo,
-    })
-      .then(function(response) {
-        if (response.data.success) {
-          window.alert('문자가 발송되었습니다');
-          handleNext();
-        } else {
-          window.alert(response.data.sms_error);
-          handleNext();
-        }
-      })
-      .catch(function(error) {
-        window.alert('오류가 발생했습니다');
-      });
-  };
-
   const handleNext = () => {
-    if (activeStep === steps.length - 1) {
-      sendMessage();
-    } else {
-      setActiveStep(activeStep + 1);
-    }
+    setActiveStep(activeStep + 1);
   };
 
   const goFirst = () => {
-    setUnderstood(false);
     setActiveStep(0);
   };
 
   const handleBack = () => {
-    setUnderstood(false);
     setActiveStep(activeStep - 1);
   };
 
-  const nowUnderstand = () => {
-    setUnderstood(!understood);
-  };
-
   return (
-    <React.Fragment>
+    <>
       <CssBaseline />
       <Paper className={classes.paper}>
         <Typography component="h1" variant="h4" align="center">
@@ -130,9 +103,9 @@ export default function Checkout() {
             </Step>
           ))}
         </Stepper>
-        <React.Fragment>
+        <>
           {activeStep === steps.length ? (
-            <React.Fragment>
+            <>
               <Typography variant="h5" gutterBottom>
                 지원해 주셔서 감사합니다.
               </Typography>
@@ -144,39 +117,12 @@ export default function Checkout() {
               >
                 처음으로 돌아가기
               </Button>
-            </React.Fragment>
+            </>
           ) : (
-            <React.Fragment>
-              {getStepContent(activeStep)}
-              <div className={classes.buttons}>
-                {activeStep !== 0 && (
-                  <Button onClick={handleBack} className={classes.button}>
-                    <i class="material-icons">arrow_back</i>
-                  </Button>
-                )}
-                {understood === true ? (
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={handleNext}
-                    className={classes.button}
-                  >
-                    {activeStep === steps.length - 1 ? (
-                      <i class="material-icons">send</i>
-                    ) : (
-                      <i class="material-icons">arrow_forward</i>
-                    )}
-                  </Button>
-                ) : (
-                  <Button>
-                    <i class="material-icons">close</i>
-                  </Button>
-                )}
-              </div>
-            </React.Fragment>
+            <>{getStepContent(activeStep)}</>
           )}
-        </React.Fragment>
+        </>
       </Paper>
-    </React.Fragment>
+    </>
   );
 }
--- src/PaymentForm.js
+++ src/PaymentForm.js
@@ -1,32 +1,46 @@
 import React from 'react';
-import {
-  Checkbox,
-  Grid,
-  TextField,
-  Typography,
-  FormLabel,
-  FormControl,
-  FormGroup,
-  FormControlLabel,
-  FormHelperText,
-} from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
 
-export default function PaymentForm() {
-  const [state, setState] = React.useState({
-    frontend: false,
-    backend: false,
-    design: false,
-  });
+const PaymentForm = props => {
+  const [name, setName] = React.useState('');
+  const [studentId, setStudentId] = React.useState('');
+  const [phoneNumber, setPhoneNumber] = React.useState('');
+  const [previousDevCareer, setPreviousDevCareer] = React.useState('');
+  const [message, setMessage] = React.useState('');
 
-  const handleChange = name => event => {
-    setState({ ...state, [name]: event.target.checked });
+  const sendMessage = () => {
+    const info = {
+      name: name,
+      student_id: studentId,
+      phone_number: phoneNumber,
+      previous_dev_career: previousDevCareer,
+      message: message,
+    };
+    console.log(info);
+    axios({
+      url: 'https://infoteam_api.samhome.xyz/api/aplication',
+      method: 'POST',
+      data: info,
+    })
+      .then(function(response) {
+        if (response.data.success) {
+          window.alert('문자가 발송되었습니다');
+          props.handleNext();
+        } else {
+          window.alert(response.data.sms_error);
+        }
+      })
+      .catch(function(error) {
+        window.alert('오류가 발생했습니다');
+      });
   };
 
-  const { frontend, backend, design } = state;
-  const error = [frontend, backend, design].filter(v => v).length === 0;
-
   return (
-    <React.Fragment>
+    <>
       <Typography variant="h6" gutterBottom>
         지원자 정보
       </Typography>
@@ -35,31 +49,86 @@ export default function PaymentForm() {
       </Typography>
       <Grid container spacing={3}>
         <Grid item xs={12} md={6}>
-          <TextField required id="Name" label="이름" fullWidth />
+          <TextField
+            required
+            id="name"
+            label="이름"
+            value={name}
+            onChange={e => {
+              setName(e.target.value);
+              console.log(name);
+            }}
+            fullWidth
+          />
         </Grid>
         <Grid item xs={12} md={6}>
-          <TextField required id="studentNumber" label="학번" fullWidth />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField required id="phone" label="휴대폰 번호" fullWidth />
+          <TextField
+            required
+            id="student_id"
+            label="학번"
+            value={studentId}
+            onChange={e => {
+              setStudentId(e.target.value);
+              console.log(studentId);
+            }}
+            fullWidth
+          />
         </Grid>
         <Grid item xs={12}>
           <TextField
-            multiline
-            id="exp"
-            label="개발에 참여하신 경험이 있으시면 적어주세요"
+            required
+            id="phone_number"
+            label="휴대폰 번호"
+            value={phoneNumber}
+            onChange={e => {
+              setPhoneNumber(e.target.value);
+              console.log(phoneNumber);
+            }}
             fullWidth
           />
         </Grid>
         <Grid item xs={12}>
           <TextField
             multiline
-            id="etc"
+            id="previous_dev_career"
+            label="개발에 참여하신 경험이 있으시면 적어주세요"
+            value={previousDevCareer}
+            onChange={e => {
+              setPreviousDevCareer(e.target.value);
+              console.log(previousDevCareer);
+            }}
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            multiline
+            id="message"
             label="하고싶은 말있으시면 적어주세요"
+            value={message}
+            onChange={e => {
+              setMessage(e.target.value);
+              console.log(message);
+            }}
             fullWidth
           />
         </Grid>
       </Grid>
-    </React.Fragment>
+      <div className={props.classes.buttons}>
+        <Button onClick={props.handleBack} className={props.classes.button}>
+          <i class="material-icons">arrow_back</i>
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={sendMessage}
+          className={props.classes.button}
+        >
+          <i class="material-icons">send</i>
+        </Button>
+      </div>
+    </>
   );
-}
+};
+
+export default PaymentForm;
